1cc186d55e7ff830e010cdffb2cc7b6371ff0f16
Syahrul17458 <83635606+Syahrul17458@users.noreply.github.com>
2021-12-03 14:29:24 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -44,9 +44,9 @@ const defaultMenu = {
 │ 
 ╰────
 %readmore`.trimStart(),
-  header: '╭─「 %category 」',
-  body: '│ • %cmd %islimit %isPremium',
-  footer: '╰────\n',
+  header: '┌─❑「 %category 」',
+  body: '│ ◦➛ %cmd %islimit %isPremium',
+  footer: '└❏\n',
   after: `
 *%npmname@^%version*
 ${'```%npmdesc```'}
